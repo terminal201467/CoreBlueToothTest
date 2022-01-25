1d8028d9aaeed471727f2bb6e4191ade1291ee43
MJ <terminal201467@gmail.com>
2022-01-25 08:28:51 +0800

    Initial Commit

---
+++ CoreBlueTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreBlueTest
+//
+//  Created by Jhen Mu on 2022/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
